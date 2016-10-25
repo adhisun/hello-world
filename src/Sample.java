8b643406eaa9a18ea5ecea4f29b7b9956f478747
admin-asundaram <admin-asundaram@AWS13ITCRMTEAM.corp.logitech.com>
2016-10-25 16:34:18 -0700

    Commit from Eclipse

--- src/Sample.java
+++ src/Sample.java
@@ -3,5 +3,6 @@ public class Sample {
 public static void main(String args[]){
 	System.out.println("Hello World");
 	System.out.println("Hello World - Updated in Git");
+	System.out.println("Hello World - Updated in Eclipse - 3");
 }
 }
