14448b7ea68daa2502641540911df209ff8982e5
asundaram <asundaram@112048080NB001.corp.logitech.com>
2016-10-25 16:44:03 -0700

    Commit from LT Ec\lipse

--- src/Sample.java
+++ src/Sample.java
@@ -3,6 +3,7 @@ public class Sample {
 public static void main(String args[]){
 	System.out.println("Hello World");
 	System.out.println("Hello World - Updated in Git");
-	System.out.println("Hello World - Updated in Eclipse - 3");
+	System.out.println("Hello World - Updated in Eclipse in AWS- 3");
+	System.out.println("Hello World - Updated in Eclipse in DESKTOP - 4");
 }
 }
